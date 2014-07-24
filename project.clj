2541532e3f28611dca0728b78175a3911bdc94f1
Mike Flynn <mfflynn@gmail.com>
2014-07-24 11:11:37 -0700

    master: Bumped version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beats-sentence-to-playlist "0.9"
+(defproject beats-sentence-to-playlist "0.9.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
