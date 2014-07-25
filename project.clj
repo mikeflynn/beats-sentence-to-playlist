d30cd3ce96ec7cb52636b88cd8df4e6bf5790236
Mike Flynn <mfflynn@gmail.com>
2014-07-24 23:12:00 -0700

    master: Updated brand images and title per Beats branding requirements.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beats-sentence-to-playlist "0.9.5"
+(defproject beats-sentence-to-playlist "0.9.6"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
